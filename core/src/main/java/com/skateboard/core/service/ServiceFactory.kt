e46e6279e7901ee0c99e566af6ada6dbcb413247
haoxuan.wu <haoxuan.wu@neulion.com.cn>
2018-09-09 17:01:40 +0800

    add foundation

--- core/src/main/java/com/skateboard/core/service/ServiceFactory.kt
+++ core/src/main/java/com/skateboard/core/service/ServiceFactory.kt
@@ -9,38 +9,8 @@ import java.lang.reflect.Proxy
 object ServiceFactory
 {
     var moduleAService: ModuleAService? = null
-        get()
-        {
-            if (field == null)
-            {
-                field = Proxy.newProxyInstance(ModuleAService::class.java.classLoader, arrayOf(ModuleAService::class.java), object : InvocationHandler
-                {
-                    override fun invoke(proxy: Any?, method: Method?, args: Array<out Any>?): Any?
-                    {
-                        return null
-                    }
-                }) as ModuleAService?
-
-            }
-            return field
-        }
 
     var moduleBService: ModuleBService? = null
-        get()
-        {
-            if (field == null)
-            {
-                field = Proxy.newProxyInstance(ModuleBService::class.java.classLoader, arrayOf(ModuleBService::class.java), object : InvocationHandler
-                {
-                    override fun invoke(proxy: Any?, method: Method?, args: Array<out Any>?): Any?
-                    {
-                        return null
-                    }
-                }) as ModuleBService?
-
-            }
-            return field
-        }
 
 
     fun registerModuleAService(service: ModuleAService)
